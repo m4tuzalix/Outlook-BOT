5285747699febac31466529eb0543dd61636b1b9
m4tuzalix <49403646+m4tuzalix@users.noreply.github.com>
2019-08-03 13:54:53 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from datetime import timedelta
-
-
-strdate = '2019-07-31'
-
-objDate = datetime.strptime(strdate, '%Y-%m-%d')
-final = objDate + timedelta(days=1)
-print(final.date())
